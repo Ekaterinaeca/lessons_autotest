13565341b92e8f9544c49e6309dd91615ea7ecf4
Ekaterinaeca <email@example.comandrianovaekaterina53@gmail.com>
2025-05-24 10:18:47 +0300

    create branch

---
+++ 06_lesson_2.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+try:
+    driver.get("http://uitestingplayground.com/textinput")
+
+    input_field = driver.find_element(By.ID, "newButtonName")
+    input_field.clear()
+    input_field.send_keys("SkyPro")
+
+    button = driver.find_element(By.ID, "updatingButton")
+    button.click()
+
+    WebDriverWait(driver, 10).until(
+        EC.text_to_be_present_in_element((By.ID, "updatingButton"), "SkyPro")
+    )
+
+    button_text = button.text
+    print(button_text)
+
+finally:
+    driver.quit()
